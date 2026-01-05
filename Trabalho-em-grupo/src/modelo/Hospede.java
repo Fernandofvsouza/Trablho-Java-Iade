5bf7b741e26c6c8c65f5b62e96f488ba0112e3c3
Fernando souza <fernandofvsouza@icloud.com>
2026-01-05 17:17:47 +0000

    trabalho

--- Trabalho-em-grupo/src/modelo/Hospede.java
+++ Trabalho-em-grupo/src/modelo/Hospede.java
@@ -1,7 +1,16 @@
 package modelo;
 /*
+o que é necessario para representar um hóspede?
+atributos:
+-id
+-nome
+-documento
 
 
+métodos:
+-listar hospedes()
+-procurar hospede por documento()
+-editar hospede() (nome, documento)
 */
 
 public class Hospede {
