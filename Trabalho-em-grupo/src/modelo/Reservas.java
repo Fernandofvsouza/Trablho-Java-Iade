4d7f9db4765300e919fc36ec4a8397f53b5094df
Fernando souza <fernandofvsouza@icloud.com>
2026-01-05 17:22:12 +0000

    update reservas.java

--- Trabalho-em-grupo/src/modelo/Reservas.java
+++ Trabalho-em-grupo/src/modelo/Reservas.java
@@ -1,4 +1,23 @@
 package modelo;
+/*
+O que deve conter a classe Reservas?
+atributos:
+- idReserva
+- idQuarto
+- idCliente
+- numeroHospedes
+-dataCheckIn
+-dataCheckOut
+-ativo (boolean)
+
+metodos:
+- criar reserva(): encontrar qualquer quarto livre adequado, capacidade >= numeroHospedes, escolher o mais proximo possivel do n de hospedes
+-listar reservas ativas()
+-listar reservas por quarto ()(presentes ou futuras)
+-listar reservas por hospede ()(presentes ou futuras)
+-Editar reserva() (n de hospedes e/ou datas, revalidar capacidade e conflitos)
+-cancelar reserva() (setar ativo para false)
+*/
 
 public class Reservas {
     
