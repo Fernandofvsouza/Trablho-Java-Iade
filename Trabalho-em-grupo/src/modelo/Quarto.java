d98982dd448a57a28ab8bfb888e77d591ecad170
Fernando souza <fernandofvsouza@icloud.com>
2026-01-05 17:15:21 +0000

    trabalho faculdade

---
+++ Trabalho-em-grupo/src/modelo/Quarto.java
@@ -0,0 +1,19 @@
+package modelo;
+
+/*
+Requisitos da class quarto:
+Atributos:
+- idQuarto
+- numeroQuarto
+- capacidade
+-estaOcupado
+
+Metodos:
+listarTodosOsQuartos()
+listarQuartosDisponiveis()
+ListarQuartosOcupados() (mostrar dados de ocupação do quarto)
+ListarQuartoEspecifico() (Mostar todas as reservas do quarto)
+*/
+public class Quarto {
+    
+}
